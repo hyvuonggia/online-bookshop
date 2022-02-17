d3d8ba4f7da707718838c83125a4d9ea1acfa158
hyvuonggia <hyvuonggia@gmail.com>
2022-02-17 16:06:46 +0100

    Register form and send signin link to email

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { Fragment } from 'react';
+import { ToastContainer } from 'react-bootstrap';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
         <Fragment>
             <Header />
+            <ToastContainer />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { sendSignInLinkToEmail } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { auth } from '../firebase';
+import { toast } from 'react-toastify';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const config = {
+            url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+            handleCodeInApp: true,
+        };
+
+        await sendSignInLinkToEmail(auth, email, config);
+        toast.success(`A confirmation email has been sent to ${email}`);
+
+        window.localStorage.setItem('registrationEmail', email);
+
+        setEmail('');
+    };
+
     return (
         <Container>
             <Row className='justify-content-md-center m-auto'>
-                <Col xs={12} md={6}>
-                    <Form>
+                <Col xs={12} md={5}>
+                    <h4>Register</h4>
+                    <Form onSubmit={handleSubmit} method='post'>
                         <Form.Group>
                             <Form.Control
                                 type='email'
                                 placeholder='Enter email address'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                             <Form.Text className='text-muted'>
                                 Please provide valid email
                             </Form.Text>
                         </Form.Group>
+                        <Button
+                            type='submit'
+                            variant='primary'
+                            className='mt-2'
+                        >
+                            Register
+                        </Button>
                     </Form>
                 </Col>
             </Row>
