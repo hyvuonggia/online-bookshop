db9d4a56f20045171801431fec3cd434bf3a59b9
hyvuonggia <hyvuonggia@gmail.com>
2022-02-22 19:59:14 +0100

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
         },
         role: {
             type: String,
-            default: 'subscriber',
+            default: 'user',
         },
         cart: {
             type: Array,
