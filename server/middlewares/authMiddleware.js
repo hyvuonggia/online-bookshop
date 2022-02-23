6c53c5698c27353db4baf6c394ca8896ffed3d8c
hyvuonggia <hyvuonggia@gmail.com>
2022-02-23 13:01:36 +0100

    Add admin check middleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import admin from '../firebase/index.js';
+import User from '../models/userModel.js';
 
 export const authCheck = async (req, res, next) => {
     try {
@@ -13,5 +14,21 @@ export const authCheck = async (req, res, next) => {
         res.status(401).json({
             error: 'Invalid or expired token',
         });
+    }
+};
+
+export const adminCheck = async (req, res, next) => {
+    const { email } = req.user;
+    try {
+        const admin = User.findOne({ email });
+        if (admin.role !== 'admin') {
+            res.status(403).json({
+                error: 'Access denied',
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.error(error);
     }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createUser, getCurrentUser } from '../controllers/userControllers.js';
-import { authCheck } from '../middlewares/authMiddleware.js';
+import { adminCheck, authCheck } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(authCheck, createUser);
 // router.route('/:id').put(authCheck, updateUser);
-router.route('/current-user').get(authCheck, getCurrentUser)
+router.route('/current-user').get(authCheck, getCurrentUser);
+router.route('/current-admin').get(authCheck, adminCheck, getCurrentUser);
 
 export default router;
