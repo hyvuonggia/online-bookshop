3ea74076cdcdb925dbcaf39420051885a1d9d6b0
hyvuonggia <hyvuonggia@gmail.com>
2022-02-25 10:43:09 +0100

    Update SideBarNav.jsx

--- client/src/components/sidebar/SideBarNav.jsx
+++ client/src/components/sidebar/SideBarNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './SideBarNav.scss';
 
@@ -18,23 +19,106 @@ const sideBarNavItems = [
 ];
 
 const SideBarNav = () => {
-    return (
+    const userLogin = useSelector((state) => state.userLogin);
+    const { user } = userLogin;
+
+    return user.role === 'admin' ? (
         <div className='sidebar'>
             <div className='sidebar-menu'>
                 <div className='sidebar-menu-indicator'>
-                    {sideBarNavItems.map((item, index) => (
-                        <Link
-                            to={item.to}
-                            key={index}
-                            style={{ textDecoration: 'none' }}
-                        >
-                            <div className='sidebar-menu-item'>
-                                <div className='sidebar-menu-item-text'>
-                                    {item.display}
-                                </div>
+                    <Link
+                        to='/user/history'
+                        key='history'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Dashboard
                             </div>
-                        </Link>
-                    ))}
+                        </div>
+                    </Link>
+                    <Link
+                        to='/admin/product'
+                        key='product'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Product
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        to='/admin/products'
+                        key='products'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Products
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        to='/admin/coupon'
+                        key='coupon'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>Coupon</div>
+                        </div>
+                    </Link>
+                    <Link
+                        to='/user/password'
+                        key='password'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Password
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    ) : (
+        <div className='sidebar'>
+            <div className='sidebar-menu'>
+                <div className='sidebar-menu-indicator'>
+                    <Link
+                        to='/admin/dashboard'
+                        key='history'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Dashboard
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        to='/user/wishlist'
+                        key='history'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Wishlist
+                            </div>
+                        </div>
+                    </Link>
+                    <Link
+                        to='/user/password'
+                        key='password'
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <div className='sidebar-menu-item'>
+                            <div className='sidebar-menu-item-text'>
+                                Password
+                            </div>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
