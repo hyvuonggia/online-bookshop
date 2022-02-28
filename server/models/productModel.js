671073fd52978f32950982007df620f0516ae5bd
hyvuonggia <hyvuonggia@gmail.com>
2022-02-28 10:36:40 +0100

    Create productModel.js

---
+++ server/models/productModel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        slug: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            index: true,
+        },
+        author: {
+            type: String,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+        },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true,
+        },
+        quantity: {
+            type: Number,
+        },
+        sold: {
+            type: Number,
+            default: 0,
+        },
+        images: {
+            type: Array,
+        },
+        shipping: {
+            type: String,
+            enum: ['Yes', 'No'],
+        },
+    },
+    { timestamps: true },
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export const Product
