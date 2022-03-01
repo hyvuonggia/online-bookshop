65c7049ccd5fada74c5036605e53e037c1591ee6
hyvuonggia <hyvuonggia@gmail.com>
2022-03-01 09:19:26 +0100

    Update productControllers.js

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -2,13 +2,17 @@ import slugify from 'slugify';
 import Product from '../models/productModel.js';
 
 export const createProduct = async (req, res) => {
-    req.body.slug = slugify(req.body.title);
-    if (await Product.findOne({ slug: req.body.slug })) {
-        res.status(400).send('Product already created');
-    } else {
-        const newProduct = await new Product(req.body);
-        console.log('newProduct', newProduct);
-        await newProduct.save();
-        res.json(newProduct);
+    try {
+        req.body.slug = slugify(req.body.title);
+        if (await Product.findOne({ slug: req.body.slug })) {
+            res.status(400).send('Product already created');
+        } else {
+            const newProduct = await new Product(req.body);
+            console.log('newProduct', newProduct);
+            await newProduct.save();
+            res.json(newProduct);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 };
