3e2937071cf88d221b1bb239bd4852c72151193a
hyvuonggia <hyvuonggia@gmail.com>
2022-03-04 12:53:42 +0100

    Update AdminDashboard.jsx

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductsLimit } from '../actions/productActions';
 
@@ -14,9 +15,25 @@ const AdminDashboard = () => {
     const { products } = useSelector((state) => state.getProductsLimit);
 
     return (
-        <div>
+        <div className='px-5'>
             <h1>Products</h1>
-            {JSON.stringify(products)}
+            <Row className='g-4' lg={5} md={2} sm={1}>
+                {products.map((product) => (
+                    <Col key={product._id}>
+                        <Card style={{ height: '300px' }}>
+                            <Card.Img
+                                variant='top'
+                                src={product.image.url}
+                                style={{ height: '200px', width: 'auto' }}
+                            />
+                            <Card.Body>
+                                <Card.Title>{product.title}</Card.Title>
+                                <Card.Text>{product.description}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 };
