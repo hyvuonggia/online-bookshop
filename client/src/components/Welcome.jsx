fa4d9d13a1a3845ae29201b585b69eb3a8655d1f
hyvuonggia <hyvuonggia@gmail.com>
2022-03-12 14:57:28 +0100

    Created landing page

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import '../css/Welcome.scss';
+
+const Welcome = () => {
+    return (
+        <div className='background'>
+            <Container>
+                <h1>WELCOME TO BOOKSHOP</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Welcome;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,18 @@ import { Container } from 'react-bootstrap';
 import BestSellers from '../components/BestSellers';
 import CategoryList from '../components/CategoryList';
 import NewArrivals from '../components/NewArrivals';
+import Welcome from '../components/Welcome';
 
 const Home = () => {
     return (
-        <Container className='mt-5'>
-            <NewArrivals />
-            <BestSellers />
-            <CategoryList />
-        </Container>
+        <>
+            <Welcome />
+            <Container className='mt-5'>
+                <NewArrivals />
+                <BestSellers />
+                <CategoryList />
+            </Container>
+        </>
     );
 };
 
