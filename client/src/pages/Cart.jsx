08c490f834ea2c7935c23c35135550c26597026b
hyvuonggia <hyvuonggia@gmail.com>
2022-03-18 16:01:03 +0100

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = () => {
     };
 
     const handleCheckout = () => {
-        navigate('/login?redirect=shipping');
+        navigate('/login?redirect=/shipping');
     };
 
     return (
